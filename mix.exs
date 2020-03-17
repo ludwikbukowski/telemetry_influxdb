fb35c006e5cbeb5eafbca2b8f33971f7bb2dc804
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2020-03-17 09:28:05 +0000

    Bump telemetry version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TelemetryInfluxDB.MixProject do
   def project do
     [
       app: :telemetry_influxdb,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
