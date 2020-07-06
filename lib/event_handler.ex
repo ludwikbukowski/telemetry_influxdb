da8c5339dc38986710c0c0868203df4af2dc74ec
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-07-06 16:09:47 -0700

    Report events to InfluxDB in batches
    - Introduces a new BatchReporter that collects formatted line protocol "events" to be published (up to the maximum batch_size configuration value) when events are being emitted quickly, but can scale down to publishing single events when they are emitted more slowly.
    - Introduces new configuration value :batch_size which is the maximum number of events to send to InfluxDB in a single batch (defaults to 1: no batching)
    - Introduces a new Publisher behavior with HTTP and UDP implementations. The Publisher is now responsible for sending events to InfluxDB based on the protocol specified in the config.
    - Start new EventHandler and publisher-specific GenServers at start time.
    - EventHandler is no longer responsible for sending events to InfluxDB. It now enqueues events with their corresponding config ([{event, config}]) in the BatchReporter. The BatchReporter calls the BatchHandler which uses the configured Publisher to publish/send the events to InfluxDB.
    - Removed now-obsolete protocol-specific EventHandlers.
    - Introduces a new BatchHandler that "handles" publishing batches of line protocol as a single newline separated body
    
    Co-authored-by: Iris Scholten <ischolten.is@gmail.com>
    Co-authored-by: Delmer Reed <delmer814+1@gmail.com>
    Co-authored-by: Palak Bhojani <palak@influxdata.com>
    Co-authored-by: Marc Delagrammatikas <delagrammatikas@gmail.com>

---
+++ lib/event_handler.ex
@@ -0,0 +1,84 @@
+defmodule TelemetryInfluxDB.EventHandler do
+  @moduledoc false
+
+  use GenServer
+
+  alias TelemetryInfluxDB, as: InfluxDB
+  alias TelemetryInfluxDB.BatchReporter
+  alias TelemetryInfluxDB.Formatter
+
+  @spec start_link(InfluxDB.config()) :: GenServer.on_start()
+  def start_link(config) do
+    GenServer.start_link(__MODULE__, config)
+  end
+
+  @impl GenServer
+  def init(config) do
+    Process.flag(:trap_exit, true)
+    config = config.publisher.add_config(config)
+    handler_ids = attach_events(config.events, config)
+
+    {:ok, %{handler_ids: handler_ids}}
+  end
+
+  @impl GenServer
+  def handle_info({:EXIT, _pid, reason}, state) do
+    {:stop, reason, state}
+  end
+
+  @spec attach_events([InfluxDB.event_spec()], InfluxDB.config()) :: [InfluxDB.handler_id()]
+  defp attach_events(event_specs, config) do
+    Enum.map(event_specs, &attach_event(&1, config))
+  end
+
+  @spec attach_event(InfluxDB.event_spec(), InfluxDB.config()) :: InfluxDB.handler_id()
+  defp attach_event(event_spec, config) do
+    telemetry_config =
+      config
+      |> Map.delete(:events)
+      |> Map.put(:metadata_tag_keys, event_spec[:metadata_tag_keys] || [])
+
+    handler_id = handler_id(event_spec.name, config.reporter_name)
+
+    :ok =
+      :telemetry.attach(handler_id, event_spec.name, &__MODULE__.handle_event/4, telemetry_config)
+
+    handler_id
+  end
+
+  @spec handle_event(
+          InfluxDB.event_name(),
+          InfluxDB.event_measurements(),
+          InfluxDB.event_metadata(),
+          InfluxDB.config()
+        ) :: :ok
+  def handle_event(event, measurements, metadata, config) do
+    event_tags = Map.get(metadata, :tags, %{})
+    event_metadatas = Map.take(metadata, config.metadata_tag_keys)
+
+    tags =
+      Map.merge(config.tags, event_tags)
+      |> Map.merge(event_metadatas)
+
+    formatted_event = Formatter.format(event, measurements, tags)
+
+    BatchReporter.get_name(config)
+    |> BatchReporter.enqueue_event({formatted_event, config})
+
+    :ok
+  end
+
+  @spec handler_id(InfluxDB.event_name(), binary()) :: InfluxDB.handler_id()
+  defp handler_id(event_name, prefix) do
+    {__MODULE__, event_name, prefix}
+  end
+
+  @impl GenServer
+  def terminate(_reason, state) do
+    for handler_id <- state.handler_ids do
+      :telemetry.detach(handler_id)
+    end
+
+    :ok
+  end
+end
