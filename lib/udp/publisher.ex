8213a92cc6a7c487c62080eda1ae1fb2f1f174b0
Randy Coulman <rcoulman@influxdata.com>
2020-07-09 15:19:13 -0700

    refactor: use new EventHandler
    - Introduce a new `Publisher` behavior with HTTP and UDP implemenations.
    
    - Use the publisher in the new EventHandler
    
    - Start new EventHandler and publisher-specific GenServers at start time.
    
    - Remove now-obsolete protocol-specific EventHandlers.
    
    Co-authored-by: Delmer Reed <delmer814+1@gmail.com>

---
+++ lib/udp/publisher.ex
@@ -0,0 +1,29 @@
+defmodule TelemetryInfluxDB.UDP.Publisher do
+  @moduledoc false
+  alias TelemetryInfluxDB, as: InfluxDB
+  alias TelemetryInfluxDB.UDP.Socket
+  alias TelemetryInfluxDB.UDP.Connector
+
+  require Logger
+
+  @behaviour InfluxDB.Publisher
+
+  @impl InfluxDB.Publisher
+  def add_config(config) do
+    config
+  end
+
+  @impl InfluxDB.Publisher
+  def publish(formatted_event, config) do
+    udp = Connector.get_udp(config.reporter_name)
+    packet = formatted_event <> "\n"
+
+    case Socket.send(udp, packet) do
+      :ok ->
+        :ok
+
+      {:error, reason} ->
+        Connector.udp_error(config.reporter_name, udp, reason)
+    end
+  end
+end
